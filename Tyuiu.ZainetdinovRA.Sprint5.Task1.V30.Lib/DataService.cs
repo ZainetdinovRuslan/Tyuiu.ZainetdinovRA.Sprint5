3bd9dc4345a9a209d8415e15ed1012066c034c6f
ZainetdinovRuslan <rosiper123@icloud.com>
2024-11-30 03:04:49 +0500

    Task1

---
+++ Tyuiu.ZainetdinovRA.Sprint5.Task1.V30.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZainetdinovRA.Sprint5.Task1.V30.Lib
+{
+    public class DataService : ISprint5Task1V30
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.GetTempFileName();
+            int range = Math.Abs(startValue - stopValue) + 1;
+            double[] arrValues = new double[range];
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double denominator = 2 - (2 * i);
+                double resultOfFunction;
+                if (denominator == 0)
+                {
+                    resultOfFunction = 0;
+                } 
+                else
+                {
+                    resultOfFunction = Math.Round(Math.Cos(i) + (Math.Sin(i) / denominator) - (4 * i), 2);
+                }
+                arrValues[i - startValue] = resultOfFunction;
+            }
+            File.WriteAllText(path, string.Join('\n', arrValues));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ZainetdinovRA.Sprint5.Task1.V30/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Tyuiu.ZainetdinovRA.Sprint5.Task1.V30.Lib;
+namespace Tyuiu.ZainetdinovRA.Sprint5.Task1.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Спринт #5 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Произвести проверку деления на ноль. При делении на ноль вернуть        *");
+            Console.WriteLine("* значение 0. Результат сохранить в текстовый файл OutPutFileTask1.txt и  *");
+            Console.WriteLine("* вывести на консоль в таблицу. Значения округлить до двух знаков после   *");
+            Console.WriteLine("* запятой.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            int range = Math.Abs(startValue - stopValue) + 1;
+            string result = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] fileContents = File.ReadAllLines(result);
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                Console.WriteLine($"{i} = {fileContents[i - startValue]}");
+            }
+
+        }
+    }
+}
