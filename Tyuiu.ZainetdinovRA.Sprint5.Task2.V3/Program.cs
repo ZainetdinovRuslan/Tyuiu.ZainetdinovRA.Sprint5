eaf482a319417643c88a241ed6b1b569909484a8
ZainetdinovRuslan <rosiper123@icloud.com>
2024-12-27 19:57:20 +0500

    Task2

--- Tyuiu.ZainetdinovRA.Sprint5.Task2.V3/Program.cs
+++ Tyuiu.ZainetdinovRA.Sprint5.Task2.V3/Program.cs
@@ -10,6 +10,22 @@ namespace Tyuiu.ZainetdinovRA.Sprint5.Task2.V3
         {
             Console.OutputEncoding = Encoding.UTF8;
             DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
 
             int[,] matrix = new int[3, 3]
             {
@@ -17,6 +33,10 @@ namespace Tyuiu.ZainetdinovRA.Sprint5.Task2.V3
                 { 1, 3, 9 },
                 { 1, 2, 2 }
             };
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
 
             ds.SaveToFileTextData(matrix);
         }
