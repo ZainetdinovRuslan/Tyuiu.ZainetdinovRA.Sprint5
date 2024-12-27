bcef54127459b2cbf5a631c769580febe4201882
ZainetdinovRuslan <rosiper123@icloud.com>
2024-12-27 18:49:28 +0500

    Task0

--- Tyuiu.ZainetdinovRA.Sprint5.Task0.V12.Lib/DataService.cs
+++ Tyuiu.ZainetdinovRA.Sprint5.Task0.V12.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ZainetdinovRA.Sprint5.Task0.V12.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double y = Math.Pow(x, 3) / 2 * Math.Pow(x + 5, 2);
+            double y = Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2));
 
             string result = Math.Round(y, 3).ToString();
 
